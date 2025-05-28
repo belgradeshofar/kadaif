65974833f1d9c2b8d5535bfa6e0207623a8e37dd
BelgradeShofar <milospetrovictekstoteka@gmail.com>
2025-05-28 19:27:45 +0200

    Fallback ESLint config for Vercel compatibility

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ['next', 'next/core-web-vitals'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+  },
+};
